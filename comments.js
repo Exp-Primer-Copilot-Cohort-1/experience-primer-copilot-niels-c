e6ed03b11fc51ace44a051e43bcf483b33a01dcd
Niels <niels.thiemann@gmx.de>
2023-11-26 16:34:10 +0100

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,12 +1,23 @@
 // Create Web server
-import express from 'express';
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-// Create a static file server
-app.use(express.static('public'));
+// Enable json body parsing of application/json
+app.use(express.json())
+
+// Create a GET handler
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// Create a POST handler
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.send(req.body)
+})
 
 // Start the Web server
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+    console.log(`Example app listening at http://localhost:${port}`)
+})
